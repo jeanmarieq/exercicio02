5e02892ada057abfdf08dd6cc4d8d180b10dd9b7
Jeanmarie Quijada <jeanmarie.q@gmail.com>
2021-03-18 23:40:14 -0300

    component e service e api

---
+++ exercicio02/src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../service/usuarios.service';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent implements OnInit {
+
+ usuarios: any = [];
+
+  constructor(usuariosService : UsuariosService) {
+    
+    usuariosService.getTodos()
+    .subscribe(usuarios => this.usuarios = usuarios);
+   }
+
+  ngOnInit(): void {
+   
+  }
+
+}
